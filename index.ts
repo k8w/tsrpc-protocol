947004f91fd12317ed30cf1d3f392da1f4c8ae98
wzy <twoeo@qq.com>
2017-09-27 11:20:16 +0800

    no src

--- index.ts
+++
@@ -1,6 +0,0 @@
-import TsRpcPtl from './src/TsRpcPtl';
-import TsRpcReq from './src/TsRpcReq';
-import TsRpcRes from './src/TsRpcRes';
-import TsRpcError from './src/TsRpcError';
-export default TsRpcPtl;
-export { TsRpcPtl, TsRpcReq, TsRpcRes, TsRpcError };
