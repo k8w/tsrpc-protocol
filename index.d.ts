980364d48e502cdd32d1f501414e544e2c668451
wzy <twoeo@qq.com>
2017-10-10 15:49:21 +0800

    __filename

---
+++ index.d.ts
@@ -0,0 +1,8 @@
+import TsRpcPtl from './src/TsRpcPtl';
+import TsRpcError from './src/TsRpcError';
+import TsRpcReq from './src/TsRpcReq';
+import TsRpcRes from './src/TsRpcRes';
+export { TsRpcPtl, TsRpcError, TsRpcReq, TsRpcRes };
+export default TsRpcPtl;
+
+declare var __dirname: string;
